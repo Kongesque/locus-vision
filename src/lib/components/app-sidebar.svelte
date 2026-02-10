68eef643ed82c2c4744f4f67b5b843ad06cadd61
Kongesque <kongesque@gmail.com>
2026-02-10 12:57:34 +0700

    feat: add Echo SVG and Badge components, and update app sidebar with new navigation items.

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -1,12 +1,77 @@
 <script lang="ts">
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import { Home, Radio, ScanSearch, Settings } from '@lucide/svelte';
+	import Echo from '$lib/components/svg/echo.svelte';
+	import { Badge } from '$lib/components/ui/badge/index.js';
 </script>
 
 <Sidebar.Root>
-	<Sidebar.Header />
+	<Sidebar.Header>
+		<Sidebar.Menu>
+			<Sidebar.MenuItem>
+				<Sidebar.MenuButton class="pointer-events-none data-[slot=sidebar-menu-button]:!p-1">
+					<Echo class="!size-6" />
+					<span class="text-base font-semibold">Locus</span>
+					<Badge variant="secondary">Beta</Badge>
+				</Sidebar.MenuButton>
+			</Sidebar.MenuItem>
+		</Sidebar.Menu>
+	</Sidebar.Header>
 	<Sidebar.Content>
-		<Sidebar.Group />
-		<Sidebar.Group />
+		<Sidebar.Group>
+			<Sidebar.Menu>
+				<Sidebar.MenuItem>
+					<Sidebar.MenuButton>
+						{#snippet child({ props })}
+							<a href="/" {...props}>
+								<Home />
+								<span>Home</span>
+							</a>
+						{/snippet}
+					</Sidebar.MenuButton>
+				</Sidebar.MenuItem>
+			</Sidebar.Menu>
+		</Sidebar.Group>
+		<Sidebar.Group>
+			<Sidebar.GroupLabel>Workspace</Sidebar.GroupLabel>
+			<Sidebar.Menu>
+				<Sidebar.MenuItem>
+					<Sidebar.MenuButton>
+						{#snippet child({ props })}
+							<a href="/livestream" {...props}>
+								<Radio />
+								<span>Live Stream</span>
+							</a>
+						{/snippet}
+					</Sidebar.MenuButton>
+				</Sidebar.MenuItem>
+				<Sidebar.MenuItem>
+					<Sidebar.MenuButton tooltipContent="Video Analytics">
+						{#snippet child({ props })}
+							<a href="/video-analytics" {...props}>
+								<ScanSearch />
+								<span>Video Analytics</span>
+							</a>
+						{/snippet}
+					</Sidebar.MenuButton>
+				</Sidebar.MenuItem>
+			</Sidebar.Menu>
+		</Sidebar.Group>
 	</Sidebar.Content>
-	<Sidebar.Footer />
+
+	<Sidebar.Footer>
+		<Sidebar.Menu>
+			<Sidebar.MenuItem>
+				<Sidebar.MenuButton>
+					{#snippet child({ props })}
+						<a href="/settings" {...props}>
+							<Settings />
+							<span>Settings</span>
+						</a>
+					{/snippet}
+				</Sidebar.MenuButton>
+			</Sidebar.MenuItem>
+		</Sidebar.Menu>
+	</Sidebar.Footer>
+	<Sidebar.Rail />
 </Sidebar.Root>
