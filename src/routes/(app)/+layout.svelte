7bceb5cf046d0518184c230fd013f08c3b516305
Kongesque <kongesque@gmail.com>
2026-02-10 17:37:30 +0700

    refactor: Restructure routes into `(app)` and `(auth)` groups with nested layouts.

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import AppSidebar from '$lib/components/app-sidebar.svelte';
+	import Header from '$lib/components/header.svelte';
+
+	let { data, children } = $props();
+</script>
+
+<Sidebar.Provider open={data.sidebarOpen}>
+	<AppSidebar />
+	<main class="flex h-svh w-full flex-1 flex-col overflow-y-auto">
+		<Header />
+		{@render children?.()}
+	</main>
+</Sidebar.Provider>
